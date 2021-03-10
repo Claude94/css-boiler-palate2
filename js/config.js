891a1ae357c79b407b5901d5d183173f53699fa9
Claude94 <1claude1994@gmail.com>
2021-03-10 16:07:47 +0900

    2222

---
+++ js/config.js
@@ -0,0 +1,12 @@
+//animate 라이브러리 설정 적용
+$('.animate').scrolla({
+    mobile: false,
+    once: false
+});
+
+//slick 라이브러리 설정 적용
+$('section.visual .slide').slick({
+    autoplay: true,
+    autoplaySpeed: 1000,
+    fade: true
+});
--- js/gnb-event.js
+++ js/gnb-event.js
@@ -1,13 +1,14 @@
-// 버튼의 요소 노드 취득
-const menubtn = document.querySelector('header .menu-open');
-const closebtn = document.querySelector('.gnb .close');
+//버튼의 요소 노드 취득
+const menuBtn = document.querySelector('header .menu-open');
+const closeBtn = document.querySelector('.gnb .close');
 
 const gnb = document.querySelector('.gnb');
 
 //클릭 이벤트 생성
-menubtn.addEventListaner('click', () => {
+menuBtn.addEventListener('click', () => {
     gnb.classList.add('on');
 });
-closebtn.addEventListaner('click', () => {
+
+closeBtn.addEventListener('click', () => {
     gnb.classList.remove('on');
 });
